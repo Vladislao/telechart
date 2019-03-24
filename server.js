485d2b59008fb10f62e948bc827e0562cc66ade7
vladislao <netcemetery@yandex.ru>
2019-03-25 01:32:03 +0400

    add heroku config

---
+++ server.js
@@ -0,0 +1,18 @@
+var static = require("node-static");
+//
+// Create a node-static server instance to serve the './public' folder
+//
+var file = new static.Server("./dist");
+
+require("http")
+  .createServer(function(request, response) {
+    request
+      .addListener("end", function() {
+        //
+        // Serve files!
+        //
+        file.serve(request, response);
+      })
+      .resume();
+  })
+  .listen(8080);
