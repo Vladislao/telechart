0f48fd24df55d7c3397044600311da906016eb09
vladislao <netcemetery@yandex.ru>
2019-04-16 02:22:45 +0400

    Add missing deps

--- dist/telechart.polyfill.js
+++ dist/telechart.polyfill.js
@@ -2,7 +2,7 @@
 (function() {
   var _call = Function.call.bind(Function.call);
   var _supportsDescriptors = !!Object.defineProperty;
-  var defineProperty = function(object, name, value) {
+  var _define = function(object, name, value) {
     if (name in object) {
       return;
     }
@@ -18,15 +18,61 @@
     }
   };
 
-  defineProperty(Math, "log10", function(x) {
+  _define(Object, "assign", function(target) {
+    if (target === "undefined" || target === null) {
+      throw new TypeError("target must be an object");
+    }
+
+    var objTarget = Object(target);
+    var s, source, i, props, value, key;
+    for (s = 1; s < arguments.length; ++s) {
+      source = Object(arguments[s]);
+      props = Object.keys(source);
+      for (i = 0; i < props.length; ++i) {
+        key = props[i];
+        value = source[key];
+        if (source.propertyIsEnumerable(key)) {
+          objTarget[key] = value;
+        }
+      }
+    }
+    return objTarget;
+  });
+  _define(Math, "log10", function(x) {
     return Math.log(x) * Math.LOG10E;
   });
-  defineProperty(Array.prototype, "find", function(predicate) {
+  _define(Math, "log2", function(x) {
+    return Math.log(x) * Math.LOG2E;
+  });
+  _define(Array.prototype, "fill", function fill(value) {
+    var start;
+    if (arguments.length > 1) {
+      start = arguments[1];
+    }
+    var end;
+    if (arguments.length > 2) {
+      end = arguments[2];
+    }
+    var list = Object(this);
+    var len = list.length;
+    start = start || 0;
+    end = end || 0;
+
+    var relativeStart =
+      start < 0 ? Math.max(len + start, 0) : Math.min(start, len);
+    var relativeEnd = end < 0 ? len + end : end;
+
+    for (var i = relativeStart; i < len && i < relativeEnd; ++i) {
+      list[i] = value;
+    }
+    return list;
+  });
+  _define(Array.prototype, "find", function(predicate) {
     if (typeof predicate !== "function") {
       throw new TypeError("Array#find: predicate must be a function");
     }
-    var list = this;
-    var length = this.length;
+    var list = Object(this);
+    var length = list.length;
     var thisArg = arguments.length > 1 ? arguments[1] : null;
     for (var i = 0, value; i < length; i++) {
       value = list[i];
@@ -38,8 +84,5 @@
         return value;
       }
     }
-  });
-  defineProperty(Float32Array.prototype, "slice", function(begin, end) {
-    return new Float32Array(Array.prototype.slice.call(this, begin, end));
   });
 })();
